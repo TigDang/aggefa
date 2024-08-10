7e31adeffb1d22ddec30bd110aba3c891a0e458d
Marat Gizetdinov <gizetdinov.mf@gmail.com>
2024-08-10 19:49:12 +0300

    add widerfaces hf dataset

--- src/datamodule.py
+++ src/datamodule.py
@@ -1,7 +1,38 @@
+from typing import Callable, Optional
+
+import datasets
 import hydra
 import pytorch_lightning as pl
 from omegaconf import DictConfig
-from torch.utils.data import DataLoader, random_split
+
+
+class WIDERFACEDataset:
+    def __init__(
+        self,
+        split: str,
+        transforms: Optional[Callable] = None,
+    ):
+        dataset = datasets.load_dataset("CUHK-CSE/wider_face", trust_remote_code=True)
+        self.dataset = dataset[split]
+        self.transorms = transforms
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def get_example(self, i):
+        example = self.dataset[i]
+        # Получение изображения
+        image = example["image"]
+
+        # Получение bounding boxes
+        bboxes = example["faces"]["bbox"]
+
+        if self.transorms is not None:
+            augm_result = self.transorms(image=image, bboxes=bboxes)
+            image = augm_result["image"]
+            bboxes = augm_result["bboxes"]
+
+        return image, bboxes
 
 
 class FaceDatamodule(pl.LightningDataModule):
@@ -16,7 +47,10 @@ class FaceDatamodule(pl.LightningDataModule):
         self.valset = hydra.utils.instantiate(datasets_conf.valset)
         self.testset = hydra.utils.instantiate(datasets_conf.testset)
 
-    # def setup(self):
+    def setup(self):
+        self.train_dataloader()
+        self.train_dataloader()
+        self.train_dataloader()
 
     def train_dataloader(self):
         return hydra.utils.instantiate(
@@ -32,3 +66,8 @@ class FaceDatamodule(pl.LightningDataModule):
         return hydra.utils.instantiate(
             self.loaders.get("test_loader"), dataset=self.testset
         )
+
+
+if __name__ == "__main__":
+    dataset = WIDERFACEDataset(split="train")
+    print()
