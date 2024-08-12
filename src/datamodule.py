94408e49ef670b063c45ae2fe7fa340f863603db
Marat Gizetdinov <gizetdinov.mf@gmail.com>
2024-08-12 22:29:37 +0300

    delete widerface dataset!!!!!!

--- src/datamodule.py
+++ src/datamodule.py
@@ -44,59 +44,6 @@ class UTKFace:
         }
 
 
-class WiderfaceDataset:
-    def __init__(
-        self,
-        split: str,
-        transforms: Optional[Callable] = None,
-    ):
-        dataset = datasets.load_dataset("CUHK-CSE/wider_face", trust_remote_code=True)
-        try:
-            self.dataset = dataset[split]
-        except KeyError:
-            logging.error(
-                f"There is no such split '{split}'. Available splits is {dataset.keys()}"
-            )
-        self.transorms = transforms
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        example = self.dataset[idx]
-        # Получение изображения
-        image = example["image"]
-        image = np.array(image)
-
-        # Получение bounding boxes
-        invalid_mask = torch.tensor(example["faces"]["invalid"], dtype=torch.bool)
-        # Преобразуем список bbox в тензор
-        bbox_tensor = torch.tensor(example["faces"]["bbox"], dtype=torch.float32)
-        # Выбор валидных боксов
-        bboxes = bbox_tensor[~invalid_mask]
-
-        class_labels = ["face"] * len(bboxes)
-
-        try:
-            if self.transorms is not None:
-                augm_result = self.transorms(
-                    image=image, bboxes=bboxes, class_labels=class_labels
-                )
-                image = augm_result["image"]
-                bboxes = augm_result["bboxes"]
-                class_labels = augm_result["class_labels"]
-        except:
-            print()
-
-        bboxes = torch.tensor(bboxes)
-        if bboxes.shape != torch.Size([0]):
-            x, y, w, h = bboxes.unbind(1)
-            bboxes = torch.stack((x, y, x + w, y + h), dim=1).float()
-        else:
-            bboxes = torch.zeros((1, 4))
-        return image, [{"boxes": bboxes, "labels": torch.tensor([1] * bboxes.shape[0])}]
-
-
 class FaceDatamodule(pl.LightningDataModule):
     trainset: UTKFace
     valset: UTKFace
@@ -140,11 +87,3 @@ class FaceDatamodule(pl.LightningDataModule):
         return torch.utils.data.DataLoader(
             **self.loaders_conf.test_loader, dataset=self.testset
         )
-
-
-if __name__ == "__main__":
-    from matplotlib import pyplot as plt
-
-    dataset = WiderfaceDataset(split="train")
-    plt.imsave("img.jpg", dataset[0][0])
-    print()
