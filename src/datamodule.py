1581d7ea9566ed67cbbfa39e53f9390bb613f8fe
Marat Gizetdinov <gizetdinov.mf@gmail.com>
2024-08-14 20:05:15 +0300

    add docstrings into datamodule.py

--- src/datamodule.py
+++ src/datamodule.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import Callable, Optional
+from typing import Callable, Dict, Optional
 
 import albumentations as A
 import datasets
@@ -13,7 +13,15 @@ from PIL import Image
 
 
 class UTKFace:
-    def __init__(self, image_dir, transform=None):
+    def __init__(
+        self, image_dir: os.PathLike, transform: Optional[A.BasicTransform] = None
+    ):
+        """Dataset of faces with annotation of age and gender
+
+        Args:
+            image_dir (os.PathLike): path to folder with images
+            transform (Optional[A.BasicTransform], optional): Albumentations transforms. Defaults to None.
+        """
         self.image_dir = image_dir
         self.image_filenames = os.listdir(image_dir)
         self.transform = transform
@@ -21,7 +29,12 @@ class UTKFace:
     def __len__(self):
         return len(self.image_filenames)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Dict[torch.Tensor, torch.Tensor]:
+        """
+        Returns:
+            Dict[torch.Tensor, torch.Tensor]: dictionary with keys 'age' and 'gender' with age and gender tensors
+        """
+
         # Получение имени файла и пути к изображению
         img_name = self.image_filenames[idx]
         img_path = os.path.join(self.image_dir, img_name)
@@ -53,11 +66,20 @@ class FaceDatamodule(pl.LightningDataModule):
     def __init__(
         self,
         dataset: UTKFace,
-        train_augm: Callable,
-        val_augm: Callable,
-        test_augm: Callable,
+        train_augm: A.BasicTransform,
+        val_augm: A.BasicTransform,
+        test_augm: A.BasicTransform,
         loaders: DictConfig,
     ):
+        """Datamodule which split UTKFace dataset into the train, validation and test subsets with 0.7, 0.1 and 0.2 parts of dataset.
+
+        Args:
+            dataset (UTKFace): dataset to split
+            train_augm (A.BasicTransform): albumentations for train
+            val_augm (A.BasicTransform):  albumentations for validation
+            test_augm (A.BasicTransform):  albumentations for test
+            loaders (DictConfig): config of train, val and test dataloaders
+        """
         super(FaceDatamodule, self).__init__()
 
         trainlenght = int(len(dataset) * 0.7)
